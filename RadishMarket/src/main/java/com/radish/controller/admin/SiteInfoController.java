04b4d08e17cebb58bc7300c9696004f5dd7b4f99
Dev-RiQ <lwk525678@gmail.com>
2025-03-15 11:02:56 +0900

    UPDATE : 관리자메뉴 관리자 접근

--- RadishMarket/src/main/java/com/radish/controller/admin/SiteInfoController.java
+++ RadishMarket/src/main/java/com/radish/controller/admin/SiteInfoController.java
@@ -3,6 +3,7 @@ package com.radish.controller.admin;
 import java.io.IOException;
 
 import com.radish.frontController.Controller;
+import com.radish.util.AlertUtil;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,7 +14,10 @@ public class SiteInfoController implements Controller {
 	@Override
 	public String requestHandler(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
+		Object log = request.getSession().getAttribute("log");
+		if(log == null || Integer.parseInt(log.toString()) != -1) {
+			AlertUtil.getInstance().goBackWithAlert(response, "접근 권한이 없습니다.");
+		}
 		return "admin/siteInfo";
 	}
 
--- RadishMarket/src/main/java/com/radish/controller/user/LoginController.java
+++ RadishMarket/src/main/java/com/radish/controller/user/LoginController.java
@@ -24,6 +24,7 @@ public class LoginController implements Controller {
 		if(user_id.equals("admin") && user_pw.equals("admin")) {
 			request.getSession().setAttribute("log", -1);
 			AlertUtil.getInstance().goHomeWithAlert(response, "관리자님 로그인 성공 !");
+			return null;
 		}
 		
 		int log = UserDAO.getInstance().isLoginSuccess(user_id, user_pw);
