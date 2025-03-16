b8d0cc8096e715c90a911c83f36a582cb48e6549
Lyle <procross27@gmail.com>
2025-03-16 21:57:20 +0900

    UPDATE : 아이템 인서트 이미지 저장

--- RadishMarket/src/main/java/com/radish/controller/item/InsertItemController.java
+++ RadishMarket/src/main/java/com/radish/controller/item/InsertItemController.java
@@ -1,16 +1,20 @@
 package com.radish.controller.item;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import com.radish.dao.ItemCategoryDAO;
 import com.radish.dao.ItemDAO;
+import com.radish.dao.ItemImgDAO;
 import com.radish.dao.UserDAO;
 import com.radish.frontController.Controller;
 import com.radish.util.AlertUtil;
 import com.radish.util.DateUtil;
 import com.radish.vo.Item;
 import com.radish.vo.ItemCategory;
+import com.radish.vo.ItemImg;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -37,13 +41,26 @@ public class InsertItemController implements Controller {
 		if(request.getParameter("item_name") == null) {
 			return "item/itemInsert";
 		}
-		
+
 		int item_category_no = Integer.parseInt(request.getParameter("item_category_no"));
 		String item_name = request.getParameter("item_name");
 		String item_content = request.getParameter("item_content");
 		int item_price = Integer.parseInt(request.getParameter("item_price"));
 		String item_reg_datetime = DateUtil.getInstance().getRegDatetime();
 		Item item = new Item(log, item_category_no, item_name, item_content, item_price, item_reg_datetime, item_reg_datetime, 1, 0);
+		
+		if(item != null) {
+			String uploadFileNames = request.getParameter("user_itme_img");
+			String[] uploadFileName = uploadFileNames.split(", ");
+			List<ItemImg> list = new ArrayList<>();
+			for(String item_img : uploadFileName) {
+				list.add(new ItemImg(item.getItem_no(), item_img));
+			}
+			if(ItemImgDAO.getInstance().insertItemImg(list) == 0) {
+				AlertUtil.getInstance().goBackWithAlert(response, "서버 오류로 인해 판매글 이미지 등록에 실패했습니다.\\\\n다시 시도해주세요.");
+				return null;
+			};
+		}
 		
 		String user_nickname = UserDAO.getInstance().getAUserNicnameByUserNo(log);
 		
--- RadishMarket/src/main/java/com/radish/dao/ItemImgDAO.java
+++ RadishMarket/src/main/java/com/radish/dao/ItemImgDAO.java
@@ -1,10 +1,37 @@
 package com.radish.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+
+import com.radish.util.DBUtil;
+import com.radish.vo.ItemImg;
+
 public class ItemImgDAO {
 	private static ItemImgDAO instance;
-	private ItemImgDAO() {}
+
+	private ItemImgDAO() {
+	}
+
 	public static ItemImgDAO getInstance() {
-		if(instance == null) instance = new ItemImgDAO();
+		if (instance == null)
+			instance = new ItemImgDAO();
 		return instance;
 	}
+
+	public int insertItemImg(List<ItemImg> list) {
+		int cnt = 0;
+		try (SqlSession session = DBUtil.getInstance().openSession()) {
+			for (ItemImg itemImg : list) {
+				cnt += session.insert("insertItemImg", itemImg);
+				session.commit();
+			}
+			System.out.println(cnt + "개의 이미지 삽입");
+		} catch (Exception e) {
+			System.out.println("insertItemImg fail");
+			e.printStackTrace();
+		}
+		return cnt;
+	}
+
 }
