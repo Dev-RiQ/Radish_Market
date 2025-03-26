9643695e1107b38529461d1642e76b81b1f27d27
Lyle <procross27@gmail.com>
2025-03-26 18:21:07 +0900

    UPDATE : user profile CSS

--- RadishMarket/src/main/webapp/js/itemInfoSwiper.js
+++ RadishMarket/src/main/webapp/js/itemInfoSwiper.js
@@ -17,3 +17,21 @@ document.addEventListener("DOMContentLoaded", function () {
         console.error("Swiper 라이브러리가 정상적으로 로드되지 않았습니다.");
     }
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+  const swiperImages = document.querySelectorAll('.swiper-slide img');
+  const modal = document.getElementById('imageModal');
+  const modalImage = document.getElementById('modalImage');
+
+  swiperImages.forEach(img => {
+    img.addEventListener('click', () => {
+      modalImage.src = img.src;
+      modal.classList.remove('hidden');
+    });
+  });
+
+  modal.addEventListener('click', () => {
+    modal.classList.add('hidden');
+    modalImage.src = ''; 
+  });
+});
